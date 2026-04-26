1fa1de59213081edef4e5e01cd18119ec0a29f1c
ewertonsantanamelo-web <ewerton.santana.melo@gmail.com>
2026-04-26 09:55:57 -0300

    Reusing Instatiate

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,6 +14,8 @@ public class Program {
 	
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		System.out.println("===== TEST 1: seller findByd ====");
+		
 		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
